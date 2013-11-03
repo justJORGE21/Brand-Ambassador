2544c81fe88e8d800effae8777b1db5cf56e5595
Roshan <Roshan@Roshan-VAIO.home>
2013-11-03 01:16:56 -0600

    added new report page. currently it just display a new page only

--- BrandAmbassador/gen/com/example/brandambassador/R.java
+++ BrandAmbassador/gen/com/example/brandambassador/R.java
@@ -50,11 +50,13 @@ public final class R {
         public static final int activity_display_documents=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int add_event=0x7f030002;
+        public static final int do_report=0x7f030003;
     }
     public static final class menu {
         public static final int add_event=0x7f070000;
         public static final int display_documents=0x7f070001;
-        public static final int main=0x7f070002;
+        public static final int do_report=0x7f070002;
+        public static final int main=0x7f070003;
     }
     public static final class string {
         public static final int Addbutton=0x7f05000a;
